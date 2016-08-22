36f560e8f3f64f17bd9eef76bdd43fc87c8709a9
kumaheika <w87502047@gmail.com>
2016-08-22 15:51:46 +0800

    更新Ajax、Scroll功能

--- js/effect.js
+++ js/effect.js
@@ -1,14 +1,46 @@
 $(function () {
+  var $body = $('body');
   var nav = $(".nav_area");
   var hight_point = $(window).height();
   var fixed = "fixed";
+  var $navBTN = $(".navBTN"); // Nav選單
 
+  //主選單變化控制
   $(window).scroll(function () {
+    //獲取現在螢幕scrolltop
+    var scrollVal = $(this).scrollTop();
+    console.log(scrollVal);
+
     if( $(this).scrollTop() > 0 ) {
       nav.addClass(fixed);
     } else {
       nav.removeClass(fixed);
     }
   })
+
+  //錨點移動
+  // var $tab01 = $('div.tab01').offset().top;
+  // $('#gotab01').click(function(e) {
+  //   e.preventDefault();
+  //   var $body = $('body');
+  //   $body.animate({
+  //     scrollTop: $('div.tab01').offset().top
+  //   }, 500, "swing");
+  //   return false;
+  // });
+
+  //主選單按鈕
+  $navBTN.on("click", function (e) {
+    var $target = $(this).attr("data-target");
+    var $scrollTop = $($target).offset().top;
+    e.preventDefault();
+    anmation_Swing ($scrollTop);
+  });
+  //animate function
+  function anmation_Swing (where) {
+    $body.animate({
+      scrollTop: where
+    }, 500, "swing");
+  }
 
 });
--- js/map_point.js
+++ js/map_point.js
@@ -5,6 +5,8 @@ $(document).ready(addMarker);
 function addMarker() {
   //open sources
   var JsonURL = "https://data.kaohsiung.gov.tw/Opendata/DownLoad.aspx?Type=2&CaseNo1=AV&CaseNo2=2&FileType=1&Lang=C&FolderType=";
+  var template = $('.card');
+  $('.food_list').html("");
 
   $.ajax(
     {
@@ -16,6 +18,25 @@ function addMarker() {
         var map;
 
         for( var i=0; i<=data.length; i++ ) {
+
+          //將 ajax 的 data 塞入列表中
+          AjaxLoad (data);
+          function AjaxLoad (data) {
+            var food_list = $('.food_list'); //要放 card 的位置
+            var food_tem = template.clone(); //複製一個 card 當作 Template
+
+            //將 Ajax 資料塞入 template
+            food_tem.find('.Name').text( data[i].Name );
+            food_tem.find('.Description').text( data[i].Description );
+            food_tem.find('.Add').text( data[i].Add);
+            food_tem.find('.Opentime').text( data[i].Opentime );
+            food_tem.find('img').attr( "src", data[i].Picture1 ).attr( "alt", data[i].Name );
+
+            //將 更新過的 template 塞入 列表
+            food_list.append( food_tem );
+          }
+
+
           //環圈第一次產生
           if ( first == true ) {
             //中心點位置
@@ -46,15 +67,14 @@ function addMarker() {
           var content = '<div class=infowin><p>'+info+'</p></div>';
           attach(marker,content);
 
-          function attach(marker,content){
+          function attach (marker,content) {
               var infowindow = new google.maps.InfoWindow({
                                content: content
                       });
-              google.maps.event.addListener(marker, 'click', function() {
+              google.maps.event.addListener (marker, 'click', function() {
                       infowindow.open(marker.get('map'), marker);
                       });
-              }
-
+          }
 
         }//for結束
       },
